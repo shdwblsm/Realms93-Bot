60b71e315a46118568894aeb4b3f5eff97d116d7
shdwblsm <Sara.Lovelace@gmail.com>
2020-12-03 22:04:09 -0600

    export main script to start-bot.py

--- start-bot.py
+++ start-bot.py
@@ -1,22 +1,24 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 import time
+from datetime import datetime
 import sys
 import telnetlib
 from telnetlib import Telnet 
+import interact
+ 
+
+
+
 
 tn = telnetlib.Telnet("www.windows93.net", 8082)  
-        
+
+
+
 # initiate telnet connection for ....
 
-
-user = input("Bot Name: ")
+botName = input("Bot Name: ")
 password = input("Bot Password: ")
 host = "www.windows93.net"
+botState = "active"
 
 # Open Session
 tn.open(host, 8082)
@@ -24,28 +26,58 @@ tn.open(host, 8082)
 print("Opening Session")
 
 tn.read_until(b"Please enter your name, or \"new\" if you are new.")
-tn.write(user.encode('ascii') + b"\n")
+tn.write(botName.encode('ascii') + b"\n")
 tn.read_until(b"Please enter your password.")
 tn.write(password.encode('ascii') + b"\n")
              
 # Look in world
              
-tn.write(b"look\n")
+# tn.write(b"look\n")
 
 time.sleep(1)
 
-Output = tn.read_very_eager().decode('ascii')
+online = "Coming Online..."
 
-print(Output)
+tn.write(online.encode('ascii') + b"\n")
 
-
-             
+while True:    
+    
+    Output = tn.read_very_eager().decode('ascii')
+    now = datetime.now()
+    tstamp = now.strftime("[%m/%d/%Y %H:%M] ")
+    
+    if Output == "":
+        continue;
+        
+    else:
+           
+        
+        # Save a log
+        file = open("realms93-log", "a")
+        file.write(tstamp + Output)
+        file.close()
+        
        
-#
+        print(Output)
+        # Assess Situation
+
+     
+        takeAction = interact.assess(Output,botState,botName)
+        
+        # print('TakeAction variable is ' + takeAction)
+        
+        
+           
+        if (takeAction is None):
+            continue
+
+            
+        else:
+            tn.write(takeAction.encode('ascii') + b"\n")
+
+            print('===== GATHERING NEW INPUT =====')
+
+            continue
 
 
-# In[2]:
-
-
-tn.close()
-
+    
